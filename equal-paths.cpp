757fc861ea218e9626eb13bb1e61d2c97e078b53
Youngje Park <peroni014@gmail.com>
2025-03-22 16:07:39 -0700

    task1

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -4,15 +4,39 @@
 #endif
 
 #include "equal-paths.h"
+
+#include <algorithm>
 using namespace std;
 
 
 // You may add any prototypes of helper functions here
 
+int getHeight(Node* root) {
+
+    if (root == nullptr) return 0;
+
+    int leftH = getHeight(root->left);
+    int rightH = getHeight(root->right);
+
+    return std::max(leftH, rightH) + 1;
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if (root == nullptr) return true;
 
+    bool isValidL = equalPaths(root->left);
+    bool isValidR = equalPaths(root->right);
+
+    if (root->left != nullptr && root->right != nullptr)
+    {
+        int leftH = getHeight(root->left);
+        int rightH = getHeight(root->right);
+
+        return leftH == rightH;
+    }
+
+    return isValidL && isValidR;
 }
 
